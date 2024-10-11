71a6cec83d81096bdaf2a93236650f5c57d43bde
Bayzng <ayofe70@gmail.com>
2024-10-11 10:52:11 +0100

    New commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-12 h-12 object-contain' />
+          {/* <img src={logo} alt='logo' className='w-12 h-12 object-contain bg-green' /> */}
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Adebayo &nbsp;
             <span className='sm:block hidden'> | Software Developer.</span>
